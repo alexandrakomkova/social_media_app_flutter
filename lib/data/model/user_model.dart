203e235b7a969c109b21fde0e38a5bc341dea4ff
alexandrakomkova <sashakomkova2001@gmail.com>
2025-11-25 00:43:13 -0800

    add new fields to userModel

--- lib/data/model/user_model.dart
+++ lib/data/model/user_model.dart
@@ -1,9 +1,15 @@
 class UserModel {
   final String email;
   final String password;
+  String? username;
+  int? creationTimestamp;
+  String? bio;
 
-  const UserModel({
+  UserModel({
     required this.email,
     required this.password,
+    this.creationTimestamp,
+    this.username,
+    this.bio,
   });
 }
