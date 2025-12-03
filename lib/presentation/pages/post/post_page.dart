8db11357abbb92f0d74d43a8a410251683ac040b
alexandrakomkova <sashakomkova2001@gmail.com>
2025-12-03 02:52:04 -0800

    add comments ui to post_page

--- lib/presentation/pages/post/post_page.dart
+++ lib/presentation/pages/post/post_page.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_media_app/data/repository/post_repository_impl.dart';
 import 'package:social_media_app/domain/model/post_entity.dart';
+import 'package:social_media_app/domain/model/user_entity.dart';
 import 'package:social_media_app/presentation/pages/post/bloc/comments/comments_bloc.dart';
 import 'package:social_media_app/presentation/pages/post/bloc/post/post_bloc.dart';
+import 'package:social_media_app/presentation/widget/profile_avatar.dart';
 
 class PostPage extends StatelessWidget {
   final PostEntity postEntity;
@@ -43,23 +45,124 @@ class _PostView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(),
       body:
-          ListView(
-            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 4.0),
-            children: [
-              // post
-              _postInfo(context),
-              Divider(),
-              // comments section
+      Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(bottom: 70.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 4.0),
+                    children: [
+                      // post
+                      _postInfo(context),
+                      const Divider(),
 
-              // adding comment
-            ],
+                      BlocBuilder<CommentsBloc, CommentsState>(
+                        builder: (context, state) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              '${state.comments.length} comments',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+
+                      // comments section
+                      BlocBuilder<CommentsBloc, CommentsState>(
+                        builder: (context, state) {
+                          return ListView.builder(
+                            physics: NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: state.comments.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              var comment = state.comments[index];
+
+                              return ListTile(
+                                leading: ProfileAvatar(
+                                  radius: 20.0,
+                                  userEntity: UserEntity(
+                                      username: comment.username,
+                                      photoUrl: comment.userImageUrl
+                                  ),
+                                ),
+                                title: Text(
+                                    comment.username,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w600
+                                    )
+                                ),
+                                subtitle: Text(
+                                    comment.commentText
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ], //
+            ),
           ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SafeArea(
+              child: Container(
+                color: Theme
+                    .of(context)
+                    .scaffoldBackgroundColor,
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 12.0, vertical: 8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: BlocBuilder<CommentsBloc, CommentsState>(
+                        builder: (context, state) {
+                          return TextFormField(
+                            initialValue: state.commentText,
+                            decoration: const InputDecoration(
+                              hintText: 'Add a comment...',
+                              border: OutlineInputBorder(),
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 10.0, vertical: 10.0),
+                            ),
+                            onChanged: (value) {
+                              context.read<CommentsBloc>().add(
+                                  CommentsEvent.commentTextChanged(value));
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        // context.read<CommentsBloc>().add(AddComment(_commentController.text));
+                      },
+                      icon: const Icon(Icons.send),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+        ],
+      ),
     );
   }
 }
-
 
 Widget _postInfo(BuildContext context) {
   return BlocBuilder<PostBloc, PostState>(
