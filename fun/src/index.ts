3dedd93176f0a0d329e08fb70f5fde68add811c6
alexandrakomkova <sashakomkova2001@gmail.com>
2025-12-09 23:46:49 -0800

    add notifications for likes

--- fun/src/index.ts
+++ fun/src/index.ts
@@ -4,6 +4,69 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 const db = admin.firestore();
 
+export const sendNotificationOnLike = functions.firestore
+  .document("likes/{likeId}")
+  .onCreate(async (likeSnapshot: admin.firestore.QueryDocumentSnapshot) => {
+    const like = likeSnapshot.data();
+    if (!like) {
+      console.error("Like data not found");
+      return null;
+    }
+    const postSnapshot = await db.collection("posts").doc(like.postId).get();
+    const post = postSnapshot.data();
+
+    if (!post) {
+      console.error("Post data not found");
+      return null;
+    }
+
+    const postOwnerRef = post.userInfo as admin.firestore.DocumentReference;
+
+    // get post owner token to send notification
+    const tokensSnapshot = await postOwnerRef.collection("userToken").get();
+
+    if (tokensSnapshot.empty) {
+      console.log("Token is empty");
+      return null;
+    }
+
+    // to get username of person who liked post
+    const userSnapshot = await db.collection("users").doc(like.userId).get();
+    const user = userSnapshot.data();
+
+    if (!user) {
+      console.error("User data not found");
+      return null;
+    }
+    const username = user.username;
+
+    const promises: Promise<any>[] = [];
+
+    tokensSnapshot.forEach(async (tokenDoc) => {
+      const fcmToken = tokenDoc.id;
+      console.log(`fcmToken ${fcmToken}`);
+
+      const payload: admin.messaging.Message = {
+        notification: {
+          title: "New like!",
+          body: `${username} liked your post`,
+        },
+        token: fcmToken,
+      };
+
+      console.log(`payload: ${payload["notification"]}`);
+      const promise = admin.messaging().send(payload).catch((error) => {
+        console.log("Notification sending error:", error);
+      });
+
+      promises.push(promise);
+
+      // sending push notification
+      await Promise.all(promises);
+    });
+    return null;
+  });
+
 export const sendNotificationOnComment = functions.firestore
   .document("comments/{commentId}")
   .onCreate(async (commentSnapshot: admin.firestore.QueryDocumentSnapshot) => {
@@ -22,6 +85,7 @@ export const sendNotificationOnComment = functions.firestore
     }
 
     const postOwnerRef = post.userInfo as admin.firestore.DocumentReference;
+    // cOwnerRef = comment owner reference
     const cOwnerRef = comment.userInfo as admin.firestore.DocumentReference;
 
     // to get post owner fcmToken
